6faf00932c1eab1f3c57cad359f60c9c9ecbf3cb
Jeremie <contact@jeremieknops.com>
2020-06-29 16:41:48 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype And build apps with React and Swift</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Regarder la video</Link>
+    <Link to="/page-2/">Watch the video</Link>
   </div>
 )
 
